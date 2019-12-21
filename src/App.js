e1cce6946ec95b59ee16ad8806795659ab21880f
Vicente Plata <vplata@forhims.com>
2019-12-21 14:07:29 -0500

    Initial card back logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./App.css";
 import constants from "./constants";
+import CardBack from "./CardBack";
 import CardFront from "./CardFront";
 import { Container } from "react-bootstrap";
 
 function App() {
+  const searchParams = new URL(window.location).searchParams;
+
   return (
     <div className="App">
       <Container>
-        <CardFront imageUrl={constants.imageUrl} />
+        <CardFront lang={searchParams.get("lang")} imageUrl={constants.imageUrl} />
+        <CardBack lang={searchParams.get("lang")} to={searchParams.get("to")} />
       </Container>
     </div>
   );
